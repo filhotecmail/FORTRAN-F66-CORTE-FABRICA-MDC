4c794a2519c9dfbd14c17e6365868d64deb9284f
Carlos Alberto Dias da Silva Filho <filhotecmail@gmail.com>
2024-02-08 00:17:12 +0000

    Adicona as mensagens padrões de saída do sistema

--- q1mdc.f90
+++ q1mdc.f90
@@ -8,4 +8,14 @@ PROGRAM CORTAR_CHAPAS
     CB = 3520 
     CC = 7896  
 
+    WRITE (*,*) 'PROGRAMA CORDE DE CHAPAS PROPORCIONAIS'
+    WRITE (*, *) 'ANALISTA: CARLOS DIAS'
+    WRITE (*, *) 'DATA: 2024/07/02'
+    WRITE (*, *) ''
+    WRITE (*, *) 'Uma fábrica precisa cortar 1 chapa de alumínio 1520 metros quadrados, '
+    WRITE ( *,* )'1 chapa de madeira de 3520 metros quadrados e 1 chapa '
+    WRITE (*, *) 'de aço de 7896 metros quadrados em partes iguais, no maior tamanho possível e em proporção.'
+    WRITE (*, *) 'Quantos pedaços foram cortados e em quantos metros?'
+    WRITE (*, *) '' 
+
 END PROGRAM CORTAR_CHAPAS
